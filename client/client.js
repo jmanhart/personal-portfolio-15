f9a31a799f98e19eaa8e8343ed5d972e467be3c6
John <manhartjohn@gmail.com>
2015-11-11 07:30:19 -0600

    Loads of work done

--- client/client.js
+++ client/client.js
@@ -1,4 +1,18 @@
-if (Meteor.isServer) {
+// var tumblr = require('tumblr.js');
+// var client = tumblr.createClient({
+//       consumer_key: 'HibIRGJAg2t8P0eu1K9ndWIiJtMnoajtO2OkUa2wpoPLxF0fDC',
+//   	  consumer_secret: 'zSNtn3fzLX4CfOApbXBRXrPVbcMUkm6WTXY1fHuyh35a43ExkS',
+//   	  token: 'CYtlvXzvWnGmNZrPgzpKRwQCrM1x99vspcRYUqbMBMybTzTPk1',
+//   	  token_secret: 'OVRdAxP5hPEKWm9gjev55LNxzMm9vjP28ShmelhGz7qDn3tK54'
+// });
+
+
+
+
+
+
+
+if (Meteor.isClient) {
 	// Authenticate via OAuth
 	var tumblr = require('tumblr.js');
 	var client = tumblr.createClient({
@@ -7,10 +21,3 @@ if (Meteor.isServer) {
 	  token: 'CYtlvXzvWnGmNZrPgzpKRwQCrM1x99vspcRYUqbMBMybTzTPk1',
 	  token_secret: 'OVRdAxP5hPEKWm9gjev55LNxzMm9vjP28ShmelhGz7qDn3tK54'
 	});
-
-if (Meteor.isClient) {
-    Meteor.call("client.posts", function(error, results) {
-        console.log(results.content); //results.data should be a JSON object
-    });
-}
-
--- client/templates/nav/nav.js
+++ client/templates/nav/nav.js
@@ -1,7 +1,7 @@
 function init() {
     window.addEventListener('scroll', function(e){
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 300,
+            shrinkOn = 150,
             header = document.querySelector("header");
         if (distanceY > shrinkOn) {
             classie.add(header,"smaller");
