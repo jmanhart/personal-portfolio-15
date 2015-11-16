1da1af9140e1e1d97ff46c0dcbbcb9fdb962b90e
Zak Lampert <zaklampert@gmail.com>
2015-11-15 22:09:54 -0600

    get tumblr posts via http -- it works!

--- client/templates/blog/blog.js
+++ client/templates/blog/blog.js
@@ -1,8 +1,27 @@
-Template.blog.helpers({
+Template.blog.rendered = function(){
+	console.log('getting posts')
 
-    getTumblr: function getTumblr() {
-        if (Meteor.isClient) {
-            return Meteor.call('getTumblr');
-        }
-    }
-});
+
+      Meteor.call('getTumblr', function(err, res){
+      	if(!err){
+			var posts = res.data.response.posts 
+
+			console.log(posts)
+      		Session.set('posts', posts)
+      	}else{
+      		console.log(err)
+      	}
+      });
+        
+    
+}
+
+
+Template.blog.helpers({
+	'posts':function(){
+		return Session.get('posts')
+	},
+	'dateFormatted':function(){
+		return moment(this.date).calendar()
+	}
+})
--- server/tumblrfeed.js
+++ server/tumblrfeed.js
@@ -1,27 +1,11 @@
 if (Meteor.isServer) {
     Meteor.methods({
-        'getTumblr': function getTumblr() {
-            var tumblr = Meteor.npmRequire('tumblr');
+        'getTumblr': function() {
+           return HTTP.get('http://api.tumblr.com/v2/blog/jmanhart.tumblr.com/posts?api_key=HibIRGJAg2t8P0eu1K9ndWIiJtMnoajtO2OkUa2wpoPLxF0fDC'
+           )
 
-            var oauth = {
-                consumer_key: 'HibIRGJAg2t8P0eu1K9ndWIiJtMnoajtO2OkUa2wpoPLxF0fDC',
-                consumer_secret: 'zSNtn3fzLX4CfOApbXBRXrPVbcMUkm6WTXY1fHuyh35a43ExkS',
-                token: 'CYtlvXzvWnGmNZrPgzpKRwQCrM1x99vspcRYUqbMBMybTzTPk1',
-                token_secret: 'OVRdAxP5hPEKWm9gjev55LNxzMm9vjP28ShmelhGz7qDn3tK54'
-            };
-            var blog = new tumblr.Blog('jmanhart.tumblr.com', oauth);
-
-            blog.text({limit: 2}, function(error, response) {
-                if (error) {
-                    throw new Error(error);
-                }
-
-                //TODO: return response.posts when the object isn't empty I.E. return response.posts
-                console.log(response.posts);
-            });
-
-
-        }
+       }
+        
     })
 
 }
