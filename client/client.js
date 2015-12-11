9c7960320b6c07d6c1f09fa7e65bc2e10680d031
John <manhartjohn@gmail.com>
2015-12-10 19:39:12 -0600

    On track

--- client/client.js
+++ client/client.js
@@ -1,20 +1,20 @@
-sImageBox.open('images/rtj.jpg', {
-    originalHeight: false, // image will be responsive you can enable original height
-    originalWidth: false, // image will be responsive you can enable original width
-    animation: '' //image entry animation (scale, fadeIn, zoomIn, slideInDown)
-});
+// sImageBox.open('images/rtj.jpg', {
+//     originalHeight: false, // image will be responsive you can enable original height
+//     originalWidth: false, // image will be responsive you can enable original width
+//     animation: '' //image entry animation (scale, fadeIn, zoomIn, slideInDown)
+// });
 
 
-Template.design.events({
-    'click .js-open-popup': function (e) {
-        var imgPath = $(e.currentTarget).data('image');
-        if (imgPath) {
-            sImageBox.open(imgPath, {
-                animation: 'zoomIn'
-            });
-        }
-    }
-});
+// Template.design.events({
+//     'click .js-open-popup': function (e) {
+//         var imgPath = $(e.currentTarget).data('image');
+//         if (imgPath) {
+//             sImageBox.open(imgPath, {
+//                 animation: 'zoomIn'
+//             });
+//         }
+//     }
+// });
 
 
 $(window).scroll(function() {
