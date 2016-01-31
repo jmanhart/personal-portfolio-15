5a92688c483dde65ae056ef1f16f9bf1f2ea512c
John <manhartjohn@gmail.com>
2016-01-30 22:55:31 -0600

    Work and work

--- client/client.js
+++ client/client.js
@@ -10,3 +10,28 @@
 
 
 
+// we use touchstart and touchend events on buttons
+var enterEvent = "touchstart";
+var leaveEvent = "touchend";
+// we use mousedown and mouseup events on big items
+var enterBigEvent = "mousedown";
+var leaveBigEvent = "mouseup";
+if(!("ontouchstart" in window)){
+    // if no touch we use mouseenter and mouseleave events on buttons and big items
+    enterEvent = enterBigEvent = "mouseenter";
+    leaveEvent = leaveBigEvent = "mouseleave";
+}
+
+$(".button").on(enterEvent, function(e){
+    // code here
+});
+$(".button").on(leaveEvent, function(e){
+    // code here
+});
+
+$(".big-item").on(enterBigEvent, function(e){
+    // code here
+});
+$(".big-item").on(leaveBigEvent, function(e){
+    // code here
+});
